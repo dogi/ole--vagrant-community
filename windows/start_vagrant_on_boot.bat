5fb5b7d29c66813f53b8eb6394ae3fa90996dfbe
pranesh-s <pran1994@gmail.com>
2016-08-09 13:43:38 -0400

    fixed potential issues

--- windows/start_vagrant_on_boot.bat
+++ windows/start_vagrant_on_boot.bat
@@ -1,9 +1,6 @@
 ECHO OFF
 start cmd /c copy "C:\Users\%USERNAME%\ole--vagrant-community\windows\start_vagrant_on_boot.bat" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-<<<<<<< HEAD
 cd "C:\Users\%USERNAME%\ole--vagrant-community\windows"
-=======
->>>>>>> upstream/master
 cd /d %~dp0
 for /f "tokens=2* delims= " %%F IN ('vagrant status ^| find /I "default"') DO (SET "STATE=%%F%%G")
 
