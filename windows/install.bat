5c8db4867aeed723f19408d8e0be02a6bd7d6967
Fanis Gkaragkanis <FanisGk@users.noreply.github.com>
2016-09-19 22:45:23 +0300

    Updated install.bat (#36)

--- windows/install.bat
+++ windows/install.bat
@@ -22,5 +22,14 @@ CD /D "%~dp0"
 REM  Check for Virtualization and Install programs if enabled
 powershell -ExecutionPolicy bypass -Command "& {$a = (Get-CimInstance -ClassName win32_processor -Property Name, SecondLevelAddressTranslationExtensions, VirtualizationFirmwareEnabled, VMMonitorModeExtensions); $a | Format-List Name, SecondLevelAddressTranslationExtensions, VirtualizationFirmwareEnabled, VMMonitorModeExtensions; $slat = $a.SecondLevelAddressTranslationExtensions; $virtual = $a.VirtualizationFirmwareEnabled; $vmextensions = $a.VMMonitorModeExtensions;If ($slat -eq $false){Write-Host 'BeLL-Apps is not compatible with your system. In order to install it, you need to upgrade your CPU first.';exit}Else{ If ($virtual -eq $false){'Virtualization is not enabled. In order to install BeLL-Apps, you must enable it. Helpful link: http://www.howtogeek.com/213795/how-to-enable-intel-vt-x-in-your-computers-bios-or-uefi-firmware/'}If ($virtual -eq $false){"Virtualization is not enabled. In order to install BeLL-Apps, you must enable it. Helpful link: http://www.howtogeek.com/213795/how-to-enable-intel-vt-x-in-your-computers-bios-or-uefi-firmware/";exit}}(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))) >$null 2>&1;RefreshEnv;choco install bonjour, git, virtualbox, vagrant -y -allowEmptyChecksums;RefreshEnv};
 
-echo Installation Completed
-pause
+set git= dogi
+set /p git= "Enter your git username: "
+cd /D "C:\Users\%USERNAME%"
+git clone https://github.com/%git%/ole--vagrant-community.git
+
+cd ole--vagrant-community/windows
+start start_vagrant_on_boot.bat 
+start create_desktop_icon.bat
+
+exit
+
