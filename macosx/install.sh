b54ca542865758b7f47594472fae07b46113980e
Dinh Bao <hikaruit15@gmail.com>
2016-12-08 19:14:26 -0500

    set up for virtual machine start when user login

--- macosx/install.sh
+++ macosx/install.sh
@@ -12,14 +12,16 @@ brew cask install vagrant
 brew cask install virtualbox
 
 cd ~
-git clone https://github.com/dogi/ole--vagrant-community.git
+git clone https://github.com/hikaruit15/ole--vagrant-community.git
 cd ole--vagrant-community
 vagrant up
 
+#start virtual machine when user log in
 #sudo bash macosx/installvagrantboot.sh
+mv ~/ole--vagrant-community/macosx/com.ole.virtualboxboot.plist /Users/${USER}/Library/LaunchAgents/
 
 #place Icon into Dock
-mv ~/BellCommunity.app /Applications/
+mv ~/ole--vagrant-community/macosx/BellCommunity.app /Applications/
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/BellCommunity.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 # Reset Dock
 killall Dock
