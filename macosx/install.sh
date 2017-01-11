8edf73bfd56202c4cd0b68fbd95c87abba7652f3
Dinh Bao <hikaruit15@gmail.com>
2017-01-11 14:44:56 -0700

    Check if icon already exists and using cp

--- macosx/install.sh
+++ macosx/install.sh
@@ -32,8 +32,12 @@ mkdir -p /Users/${USER}/Library/LaunchAgents
 cp ~/ole--vagrant-community/macosx/com.ole.virtualboxboot.plist /Users/${USER}/Library/LaunchAgents/
 
 #place Icon into Dock
-mv ~/ole--vagrant-community/macosx/BellCommunity.app /Applications/
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/BellCommunity.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+CHECK_BELL_APP='defaults read com.apple.dock persistent-apps | grep BellCommunity.app'
+if [ "$CHECK_BELL_APP" = "" ]; then
+	cp ~/ole--vagrant-community/macosx/BellCommunity.app /Applications/
+	defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/BellCommunity.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+fi
+
 # Reset Dock
 killall Dock
 
