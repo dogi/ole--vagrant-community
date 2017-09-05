78cb6a28e39e7eb988d1668ea033912b2a110f6b
dogi <dogi@users.noreply.github.com>
2017-09-05 12:35:33 -0400

    big reboot for Madagascar (#57)

--- windows/uninstall.bat
+++ windows/uninstall.bat
@@ -1,4 +1,9 @@
 @echo off
+
+REM Set variables
+set repo=ole--vagrant-community
+set port=5984
+
 color 0b
 echo This script will uninstall BeLL-Apps from your computer 
 REM Get Admin For Batch File
@@ -17,24 +22,18 @@ if '%errorlevel%' NEQ '0' (
 )
 pushd "%CD%"
 CD /D "%~dp0"
-powershell -ExecutionPolicy bypass -Command "& {Write-Host 'Uninstalling Bonjour...'; choco uninstall bonjour -y; $vm = Read-Host 'Do you want to remove the virtual machine? (Y)es, (N)o'; if ($vm.ToUpper() -eq 'Y' -or $vm -eq '') {C:\HashiCorp\Vagrant\bin\vagrant.exe destroy community -f}$box = Read-Host 'Do you want to remove also all the files? (Y)es, (N)o'; if ($box.ToUpper() -eq 'Y' -or $box -eq '') {C:\HashiCorp\Vagrant\bin\vagrant.exe box remove ole/jessie64 -f} choco uninstall vagrant -y; $vb = Read-Host 'Are you sure you want to remove Virtualbox? (Y)es, (N)o'; if ($vb.ToUpper() -eq 'Y' -or $vb -eq '') {choco uninstall virtualbox -y} $git = Read-Host 'Are you sure you want to remove Git? (Y)es, (N)o'; if ($git.ToUpper() -eq 'Y' -or $git -eq '') {choco uninstall git, git.install -y; Remove-Item $env:ALLUSERSPROFILE\git -Force -Recurse} $ff = Read-Host 'Are you sure you want to remove Firefox? (Y)es, (N)o'; if ($ff.ToUpper() -eq 'Y' -or $ff -eq '') {choco uninstall firefox -y} Remove-Item C:\Users\*\Desktop\MyBeLL.lnk -Force; $ch = Read-Host 'Are you sure you want to uninstall Chocolatey? (Y)es, (N)o'; if ($ch.ToUpper() -eq 'Y' -or $ch -eq '') {choco uninstall chocolatey -y;exit 5}}"
+powershell -ExecutionPolicy bypass -Command "& {Write-Host 'Uninstalling Bonjour...'; choco uninstall bonjour -y; $vm = Read-Host 'Do you want to remove the virtual machine? (Y)es, (N)o'; $box = Read-Host 'Do you want to remove also all the files? (Y)es, (N)o'; $vb = Read-Host 'Are you sure you want to remove Virtualbox? (Y)es, (N)o'; $git = Read-Host 'Are you sure you want to remove Git? (Y)es, (N)o'; $ff = Read-Host 'Are you sure you want to remove Firefox? (Y)es, (N)o'; $ch = Read-Host 'Are you sure you want to uninstall Chocolatey? (Y)es, (N)o'; if ($vm.ToUpper() -eq 'Y' -or $vm -eq '') {C:\HashiCorp\Vagrant\bin\vagrant.exe destroy vi -f} if ($box.ToUpper() -eq 'Y' -or $box -eq '') {C:\HashiCorp\Vagrant\bin\vagrant.exe box remove ole/jessie64 -f} choco uninstall vagrant -y; if ($vb.ToUpper() -eq 'Y' -or $vb -eq '') {choco uninstall virtualbox -y} if ($git.ToUpper() -eq 'Y' -or $git -eq '') {choco uninstall git, git.install -y; Remove-Item $env:ALLUSERSPROFILE\git -Force -Recurse} if ($ff.ToUpper() -eq 'Y' -or $ff -eq '') {choco uninstall firefox -y} Remove-Item C:\Users\*\Desktop\MyBeLL.lnk -Force; if ($ch.ToUpper() -eq 'Y' -or $ch -eq '') {choco uninstall chocolatey -y;exit 5}}"
 if "%errorlevel%" equ "5"  (
 	RD /S /Q "%ALLUSERSPROFILE%\chocolatey"
 )
 
 
 
-netsh advfirewall firewall show rule name="CouchDB/HTTP(BeLL)" >nul
+netsh advfirewall firewall show rule name="CouchDB/HTTP(BeLL)+1" >nul
 if not ERRORLEVEL 1 (
-	echo Blocking Port 5984...
-	netsh advfirewall firewall delete rule name="CouchDB/HTTP(BeLL)" 
+	echo Blocking Port %port%...
+	netsh advfirewall firewall delete rule name="CouchDB/HTTP(BeLL)+1"
 )
-
-netsh advfirewall firewall show rule name="CouchDB/HTTPS(BeLL)" >nul
-if not ERRORLEVEL 1 (
-	echo Blocking Port 6984...
-	netsh advfirewall firewall delete rule name="CouchDB/HTTPS(BeLL)"
-) 
 
 IF EXIST "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_vagrant_on_boot.bat" (
 	del "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_vagrant_on_boot.bat"
@@ -42,7 +41,7 @@ IF EXIST "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Progr
 
 echo Uninstallation completed.
 pause
-echo Deleting ole--vagrant-community folder..
-RD /S /Q "C:\Users\%USERNAME%\ole--vagrant-community"
+echo Deleting %repo% folder..
+RD /S /Q "C:\Users\%USERNAME%\%repo%"
 exit
 
